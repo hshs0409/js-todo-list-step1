0770ddfcef801098e2d9c31c66c3f6da7a9488e3
Lee Hyun Se <0409hshs@gmail.com>
2021-01-27 17:48:19 +0900

    type : feat todo list에 todoItem을 키보드로 입력하여 추가하기
    LocalStroage 이용하여 Create 완성

---
+++ create-todo.js
@@ -0,0 +1,60 @@
+const todoInput = document.getElementById("new-todo-title");
+const todoList = document.getElementById("todo-list");
+const todoCount = document.querySelector("strong");
+const TODO_LS = "todo";
+
+let todos = [];
+
+const createTodo = e => {
+  let inputValue = todoInput.value.trim(); // 공백 문자열 입력 방지
+  if (!inputValue || inputValue === "") {
+    return;
+  }
+  if (e.key === "Enter") {
+    paintTodo(inputValue);
+    todoInput.value = "";
+  }
+  return;
+};
+
+const saveTodo = () => {
+  localStorage.setItem(TODO_LS, JSON.stringify(todos));
+};
+
+const getTodos = () => {
+  const savedTodos = JSON.parse(localStorage.getItem(TODO_LS));
+  if (!savedTodos) {
+    return;
+  }
+  savedTodos.forEach(savedTodo => paintTodo(savedTodo.text));
+};
+
+const paintTodo = inputValue => {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <div class="view">
+        <input class="toggle" type="checkbox"/>
+        <label class="label">${inputValue}</label>
+        <button class="destroy"></button>
+    </div>
+    <input class="edit" value="${inputValue}" />
+  `;
+
+  const newId = todos.length + 1;
+  li.id = newId;
+  todoList.appendChild(li);
+  const todoObj = {
+    todoId: newId,
+    text: inputValue,
+  };
+  todos.push(todoObj);
+  todoCount.innerText = todos.length;
+  saveTodo();
+};
+
+function init() {
+  getTodos();
+  todoInput.addEventListener("keypress", createTodo);
+}
+
+init();
